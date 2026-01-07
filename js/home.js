c8b33e5e255cc1b9f3cb35c7d3650348170a32d6
ASUS <nethmidileksha6@gmail.com>
2026-01-07 17:18:38 +0530

    feat : update scripts in cts.js file and update home page

---
+++ js/home.js
@@ -0,0 +1,20 @@
+
+   const currentDomain = window.location.origin;
+
+    window.addEventListener("message", function(event) {
+    if (event.origin !== "https://demo.trackerstay.com") return;
+    if (event.data === "goToNextPage") {
+        alert('messagfe');
+
+    window.location.href = currentDomain + "Hotel-website-template/booking.html/";
+}
+}, false);
+
+    // Optional: auto-adjust iframe height
+    window.addEventListener('message', function(e) {
+    if (e.data.type === 'iframeHeight') {
+    const iframe = document.getElementById('booking-iframe');
+    if (iframe) iframe.style.height = e.data.height + 'px';
+}
+});
+
--- js/slider.js
+++ js/slider.js
@@ -79,19 +79,3 @@ function autoSlide() {
 }
 
 
-
-    window.addEventListener("message", function(event) {
-    if (event.origin !== "https://demo.trackerstay.com") return;
-    if (event.data === "goToNextPage") {
-    window.location.href = currentDomain + "/booking/";
-}
-}, false);
-
-    // Optional: auto-adjust iframe height
-    window.addEventListener('message', function(e) {
-    if (e.data.type === 'iframeHeight') {
-    const iframe = document.getElementById('booking-iframe');
-    if (iframe) iframe.style.height = e.data.height + 'px';
-}
-});
-
